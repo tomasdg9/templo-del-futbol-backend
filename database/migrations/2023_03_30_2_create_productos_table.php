18096b749632755c274567d0181d4eef122bbbe0
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-04-02 21:44:05 -0300

    Agrego columna a tabla productos
    Columna para asociar la tabla productos con la tabla users brindada por Laravel

--- database/migrations/2023_03_30_2_create_productos_table.php
+++ database/migrations/2023_03_30_2_create_productos_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->string('estado', 20);
 
             // Llave foranea.
-            $table->foreignId('categoria_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
-
+            $table->foreignId('categoria_id')->constrained();
+            $table->foreignId('user_id')->constrained();
         });
     }
 
