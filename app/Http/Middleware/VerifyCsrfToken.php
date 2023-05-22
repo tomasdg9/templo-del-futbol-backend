efd3792046ed13e9100adfb9fe140121f5d8f571
Tomás De Giusti <62406337+tomasdg9@users.noreply.github.com>
2023-05-22 16:27:53 -0300

    generate swagger + vercel

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-
+use Closure;
 class VerifyCsrfToken extends Middleware
 {
     /**
@@ -15,8 +15,7 @@ class VerifyCsrfToken extends Middleware
         //
     ];
 	
-	public function handle($request, Closure $next)
-	{
+	public function handle($request, Closure $next){
 		$response = $next($request);
 		$response->headers->set('Access-Control-Allow-Origin', '*');
 		$response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
